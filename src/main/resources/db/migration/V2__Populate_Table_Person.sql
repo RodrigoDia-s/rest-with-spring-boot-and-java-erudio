47c06997b96ff4233ad2378f0dd101bc9a3edb73
Rodrigo <diasrod23@gmail.com>
2025-01-06 16:38:54 -0300

    chore: ajustando configurações de banco

--- src/main/resources/db/migration/V2__Populate_Table_Person.sql
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -1,2 +1,8 @@
-
-INSERT INTO `person` VALUES (3,'Casa da mãe joana','Rodrigo','Trans-não binarie','Dias'),(4,'Casa da mãe joana','Rodrigo','Trans-não binarie','Dias'),(5,'Casa da mãe joana','Rodrigo','Trans-não binarie','Dias'),(6,'Casa da mãe joana','Rodrigo','Trans-não binarie','Dias'),(7,'Casa da mãe joana','Rodrigo','Trans-não binarie','Dias'),(8,'Casa da mãe joana','Rodrigo','Trans-não binarie','Dias'),(9,'Casa da mãe joana','Rodrigo','Trans-não binarie','Dias');
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+(3, 'Rodrigo', 'Dias', 'Casa da mãe joana', 'Trans-não binarie'),
+(4, 'Rodrigo', 'Dias', 'Casa da mãe joana', 'Trans-não binarie'),
+(5, 'Rodrigo', 'Dias', 'Casa da mãe joana', 'Trans-não binarie'),
+(6, 'Rodrigo', 'Dias', 'Casa da mãe joana', 'Trans-não binarie'),
+(7, 'Rodrigo', 'Dias', 'Casa da mãe joana', 'Trans-não binarie'),
+(8, 'Rodrigo', 'Dias', 'Casa da mãe joana', 'Trans-não binarie'),
+(9, 'Rodrigo', 'Dias', 'Casa da mãe joana', 'Trans-não binarie');
