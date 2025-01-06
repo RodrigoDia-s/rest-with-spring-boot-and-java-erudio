d70098879010df263d8f013289dae855efc14ca5
Rodrigo <diasrod23@gmail.com>
2025-01-06 16:56:37 -0300

    chore: flyway config

--- src/test/java/br/com/erudio/StartupTests.java
+++ src/test/java/br/com/erudio/StartupTests.java
@@ -1,9 +1,9 @@
-package br.com.erudio.restwithspringbootandjavaerudio;
+package br.com.erudio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Startup.class)
 class StartupTests {
 
 	@Test
